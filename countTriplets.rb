91b936e391378eb4f664d475eb3416f84ec0ab15
huynhut <benerik.nguyen@gmail.com>
2020-07-13 10:51:35 +0700

    count triplets

---
+++ countTriplets.rb
@@ -0,0 +1,27 @@
+def countTriplets2(arr, r)
+  v2 = Hash.new(0)
+  v3 = Hash.new(0)
+  count = 0
+  arr.each do |k|
+    count += v3[k]
+    v3[k*r] += v2[k]
+    v2[k*r] += 1
+  end
+  p v2
+  p v3
+  count
+end
+#wrong
+def countTriplets(arr, r)
+  h = Hash.new(0)
+  arr.each do |k|
+    h[k] += 1
+  end
+  length = arr.length
+  r = length - 3
+  h.each_value do |v|
+    r = r*v
+  end
+  r
+end
+p countTriplets2([1,5,5,25,44, 125],5)
