f750da1215f33512909de4abfb5dab9f5567f9c2
huynhut <benerik.nguyen@gmail.com>
2024-04-30 10:03:10 +0700

    643. Maximum Average Subarray I

---
+++ 643. Maximum Average Subarray I.rb
@@ -0,0 +1,31 @@
+=begin
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+=end
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Float}
+def find_max_average(nums, k)
+  n = nums.size
+  s = (nums[0...k].sum).to_f
+  max = s/k
+  (1..(n - k)).each do |i|
+    s = ((s - nums[i - 1] + nums[i + k - 1])).to_f
+    av = s/k
+    max = av if av > max
+  end
+  max
+end
