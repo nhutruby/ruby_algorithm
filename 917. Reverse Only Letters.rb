ceef572be0565e4eeee801f55756d91434df8a1e
huynhut <benerik.nguyen@gmail.com>
2024-07-04 10:53:37 +0700

    leetcode

---
+++ 917. Reverse Only Letters.rb
@@ -0,0 +1,29 @@
+=begin
+Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+=end
+# @param {String} s
+# @return {String}
+def reverse_only_letters(s)
+  r = s.scan(/[A-Za-z]/)
+  s.gsub(/[a-z]/i) { r.pop }
+end
